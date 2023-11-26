631d849730aff4bb40b18176450ffc20c186f117
jinseisieko <jinseisieko@gmail.com>
2023-11-26 13:48:20 +0300

    gameplay.py modify

--- gameplay.py
+++ gameplay.py
@@ -6,8 +6,8 @@ from game_sprites import LittleBlueSquareEnemy
 class Room:
     def __init__(self, number_room):
         self.number_room = number_room
-        # self.enemies_number = number_room // random.randint(5, 11) + random.randint(1, 2)
-        self.enemies_number = 1000
+        self.enemies_number = number_room // random.randint(5, 11) + random.randint(1, 2)
+        # self.enemies_number = 1000
         self.enemies = self.generate_enemies()
 
     def generate_enemies(self):
