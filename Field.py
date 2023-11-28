fc64128c5c952a970dcc75025f08a2a7a3ad64b6
jinseisieko <jinseisieko@gmail.com>
2023-11-28 23:42:20 +0300

    background field

--- Field.py
+++ Field.py
@@ -1,18 +1,33 @@
+import random
+
 import pygame
 
-from Constants import GRAY, FIELD_WIDTH, FIELD_HEIGHT
+import ImageSprites
+from Constants import GRAY, FIELD_WIDTH, FIELD_HEIGHT, GREEN
 
 
 class Field:
     def __init__(self) -> None:
         # values
         self.field: pygame.surface = pygame.Surface((FIELD_WIDTH, FIELD_HEIGHT))
-        self.field.fill(GRAY)
+
+        self.background = pygame.surface = pygame.Surface((FIELD_WIDTH, FIELD_HEIGHT))
+        self.background.fill((158, 240, 144))
+        for _ in range(500000):
+            if random.choice([True] * 5 + [False]):
+                number_image_grass = random.randint(1, 5)
+                self.background.blit(ImageSprites.sprites[f"grass{number_image_grass}"],
+                                     (random.randint(-15, FIELD_WIDTH +15), random.randint(-15, FIELD_HEIGHT +15)))
+            else:
+                number_image_stone = random.randint(1, 1)
+                self.background.blit(ImageSprites.sprites[f"stone{number_image_stone}"],
+                                     (random.randint(-15, FIELD_WIDTH + 15), random.randint(-15, FIELD_HEIGHT +15)))
+
+        self.field.blit(self.background, (0, 0))
         # сделать его красивым
 
     def draw(self, *groups: pygame.sprite.Group) -> None:
         """draw groups in the sequence in which they are presented """
-
-        self.field.fill(GRAY)
+        self.field.blit(self.background, (0, 0))
         for group in groups:
             group.draw(self.field)
--- ImageSprites.py
+++ ImageSprites.py
@@ -1,4 +1,12 @@
+import pygame
+
 sprites: dict = {
-    'player': 'image/test.png',
-    'default_projectile': ''
+    'player': r"image/test.png",  # pygame.image.load(r"image/test.png")
+    'default_projectile': None,
+    'grass1': pygame.image.load(r"image/grasses/grass1.png"),
+    'grass2': pygame.image.load(r"image/grasses/grass2.png"),
+    'grass3': pygame.image.load(r"image/grasses/grass3.png"),
+    'grass4': pygame.image.load(r"image/grasses/grass4.png"),
+    'grass5': pygame.image.load(r"image/grasses/grass5.png"),
+    'stone1': pygame.image.load(r"image/stones/stone1.png"),
 }
