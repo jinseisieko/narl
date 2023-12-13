new coordinates calculation (works with bug)

Collisions.py
@@ -4,20 +4,21 @@ import numpy as np
 from Constants import *
 
 
-@numba.njit(fastmath=True)
+@numba.njit(fastmath=True, nogil=True, parallel=True)
 def calculate_Enemies(enemy_data, chunks_data, COLLISIONS_REPELLING):
-    for i in range(len(chunks_data)):
-        data_i = enemy_data[i]
-        for j in range(len(data_i)):
+    for i in numba.prange(len(chunks_data)):
+        data_i = chunks_data[i]
+        for j in numba.prange(len(data_i)):
             data_j = data_i[j]
-            for k in range(len(data_j)):
-                Id1 = data_j[k]
+            for k in numba.prange(len(data_j)):
+                Id1 = int(data_j[k])
                 x1, y1, half_size1 = enemy_data[Id1]
-                for a in range(k + 1, len(data_j)):
-                    Id2 = data_j[k]
+                for a in numba.prange(k + 1, len(data_j)):
+                    Id2 = int(data_j[k])
                     x2, y2, half_size2 = enemy_data[Id2]
 
                     real_dist = half_size1 ** 2 + half_size2 ** 2
+                    real_dist = np.sqrt(real_dist)
 
                     dist_x = x1 - x2
                     dist_y = y1 - y2

Enemies.py
@@ -8,8 +8,9 @@ from Constants import *
 
 
 @numba.njit(fastmath=True, nogil=True, parallel=True)
-def calculate_movement(player_x: float, player_y: float, enemy_data: np.array, speed: float, TICKS: int, fps: float, CHUNK_SIZE, FIELD_WIDTH, FIELD_HEIGHT):
-    chunks = np.zeros((CHUNK_SIZE, CHUNK_SIZE, 20))
+def calculate_movement(player_x: float, player_y: float, enemy_data: np.array, speed: float, TICKS: int, fps: float,
+                       CHUNK_SIZE, CHUNK_N_X, CHUNK_N_Y, FIELD_WIDTH, FIELD_HEIGHT):
+    chunks = np.zeros((CHUNK_N_Y, CHUNK_N_X, 20))
     for i in numba.prange(1, len(enemy_data)):
         x, y, half_size = enemy_data[i]
 
@@ -27,7 +28,7 @@ def calculate_movement(player_x: float, player_y: float, enemy_data: np.array, s
             x += dx * TICKS / (fps + 1e-10)
             y += dy * TICKS / (fps + 1e-10)
 
-        enemy_data[i] = np.array([x, y])
+        enemy_data[i] = np.array([x, y, half_size])
 
     return enemy_data, chunks
 
@@ -86,15 +87,14 @@ class Enemy(pygame.sprite.Sprite):
             pygame.draw.rect(self.image, 0, self.right_eye_rect)
 
     def update(self, arr) -> None:
-        self.x, self.y = arr[self.i]
+        self.x, self.y, _ = arr[self.i]
 
-        #self.x = max(0, min(FIELD_WIDTH - self.half_size, self.x))
-        #self.y = max(0, min(FIELD_HEIGHT - self.half_size, self.y))
-        #self.ind = [int(self.y / CHUNK_SIZE), int(self.x / CHUNK_SIZE)]
-        #if self.ind[0] != self.last_ind[0] or self.ind[1] != self.last_ind[1]:
+        # self.x = max(0, min(FIELD_WIDTH - self.half_size, self.x))
+        # self.y = max(0, min(FIELD_HEIGHT - self.half_size, self.y))
+        # self.ind = [int(self.y / CHUNK_SIZE), int(self.x / CHUNK_SIZE)]
+        # if self.ind[0] != self.last_ind[0] or self.ind[1] != self.last_ind[1]:
         #    self.chunks.move(self, self.last_ind, self.ind)
         #    self.last_ind = self.ind
-
 
         self.rect.x = self.x - self.half_size
         self.rect.y = self.y - self.half_size