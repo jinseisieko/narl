5abc275516b3bc0da1619b46064f0ff90540be4b
Andrchest <andreipolevoi220@gmail.com>
2024-02-14 00:14:35 +0300

    progress bars ready

--- source/Interface/Interface.py
+++ source/Interface/Interface.py
@@ -1,7 +1,7 @@
 """classes interfaces"""
 import pygame as pg
 
-from source.Constants import *
+from source.Calculations.Data import *
 from source.Image.InitializationForGame import get_images_for_game
 from source.Image.InitializationForItems import get_images_for_items
 from source.PlayerIndexes import *
@@ -23,11 +23,11 @@ class Interface:
                                                   "critical_coefficient",
                                                   "scatter", "damage_delay"]
 
-        self.items_surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+        self.items_surface = pg.Surface((WIDTH, HEIGHT), pg.SRCALPHA)
 
-        self.make_level_bar_background()
+        self.make_bar_background()
 
-    def make_level_bar_background(self):
+    def make_bar_background(self):
         self.level_background = pg.Surface((SIZE_PLAYER_LEVEL_INTERFACE_X,
                                             SIZE_PLAYER_LEVEL_INTERFACE_Y))
         clr_l = "#24d3e8"
@@ -37,13 +37,30 @@ class Interface:
 
         right_color = (int(clr_r[1:3], 16), int(clr_r[3:5], 16), int(clr_r[5:7], 16))
 
-        for i in range(SIZE_PLAYER_LEVEL_INTERFACE_X):
+        for i in range(SIZE_PLAYER_LEVEL_INTERFACE_Y):
             color = (
-                int(left_color[0] + (right_color[0] - left_color[0]) * i / SIZE_PLAYER_LEVEL_INTERFACE_X),
-                int(left_color[1] + (right_color[1] - left_color[1]) * i / SIZE_PLAYER_LEVEL_INTERFACE_X),
-                int(left_color[2] + (right_color[2] - left_color[2]) * i / SIZE_PLAYER_LEVEL_INTERFACE_X)
+                int(left_color[0] + (right_color[0] - left_color[0]) * i / SIZE_PLAYER_LEVEL_INTERFACE_Y),
+                int(left_color[1] + (right_color[1] - left_color[1]) * i / SIZE_PLAYER_LEVEL_INTERFACE_Y),
+                int(left_color[2] + (right_color[2] - left_color[2]) * i / SIZE_PLAYER_LEVEL_INTERFACE_Y)
             )
-            pg.draw.rect(self.level_background, color, (i, 0, 1, SIZE_PLAYER_LEVEL_INTERFACE_Y))
+            pg.draw.rect(self.level_background, color, (0, i, SIZE_PLAYER_LEVEL_INTERFACE_X, 1))
+
+        self.wave_background = pg.Surface((SIZE_WAVE_INTERFACE_X,
+                                           SIZE_WAVE_INTERFACE_Y))
+        clr_l = "#d67a00"
+        clr_r = "#00d642"
+
+        left_color = (int(clr_l[1:3], 16), int(clr_l[3:5], 16), int(clr_l[5:7], 16))
+
+        right_color = (int(clr_r[1:3], 16), int(clr_r[3:5], 16), int(clr_r[5:7], 16))
+
+        for i in range(SIZE_WAVE_INTERFACE_Y):
+            color = (
+                int(left_color[0] + (right_color[0] - left_color[0]) * i / SIZE_WAVE_INTERFACE_Y),
+                int(left_color[1] + (right_color[1] - left_color[1]) * i / SIZE_WAVE_INTERFACE_Y),
+                int(left_color[2] + (right_color[2] - left_color[2]) * i / SIZE_WAVE_INTERFACE_Y)
+            )
+            pg.draw.rect(self.wave_background, color, (0, i, SIZE_WAVE_INTERFACE_X, 1))
 
     def calc(self):
         self.ratio_hp = self.player[0][hp] / self.player[0][max_hp]
@@ -55,37 +72,54 @@ class Interface:
                 if not (name in get_images_for_items()):
                     name = 'no-image'
                 self.items_surface.blit(get_images_for_items()[name],
-                                        (WIDTH - 37 * NUMBER_OF_ITEMS - 10 + j * 37, 10 + i * 37))
+                                        (WIDTH - 37 * NUMBER_OF_ITEMS - 10 + j * 37 - 50, 10 + i * 37))
 
     def draw(self, screen: pg.Surface):
-        # draw hp
+        # draw bars
 
-        screen.blit(self.level_background, (COORD_PLAYER_LEVEL_INTERFACE_X, COORD_PLAYER_LEVEL_INTERFACE_Y))
+        screen.blit(self.level_background, (COORD_PLAYER_LEVEL_INTERFACE_X + 3, COORD_PLAYER_LEVEL_INTERFACE_Y + 3))
+        screen.blit(self.wave_background, (COORD_WAVE_INTERFACE_X + 3, COORD_WAVE_INTERFACE_Y + 3))
 
-        pygame.draw.rect(screen, "gold", (
-            COORD_WAVE_INTERFACE_X, COORD_WAVE_INTERFACE_Y, SIZE_WAVE_INTERFACE_X, SIZE_WAVE_INTERFACE_Y),
-                         border_radius=3)
+        pg.draw.rect(screen, (0, 0, 0),
+                     (COORD_PLAYER_LEVEL_INTERFACE_X, COORD_PLAYER_LEVEL_INTERFACE_Y,
+                      SIZE_PLAYER_LEVEL_INTERFACE_X + 6, SIZE_PLAYER_LEVEL_INTERFACE_Y + 6),
+                     border_radius=3, width=4)
 
-        pygame.draw.rect(screen, (255, 255, 255),
-                         (COORD_HP_INTERFACE_X, COORD_HP_INTERFACE_Y, SIZE_HP_INTERFACE_X, SIZE_HP_INTERFACE_Y),
-                         border_radius=3)
+        pg.draw.rect(screen, (0, 0, 0),
+                     (COORD_WAVE_INTERFACE_X, COORD_WAVE_INTERFACE_Y,
+                      SIZE_WAVE_INTERFACE_X + 6, SIZE_WAVE_INTERFACE_Y + 6),
+                     border_radius=3, width=4)
 
-        pygame.draw.rect(screen, "#a3001d",
-                         (COORD_HP_INTERFACE_X, COORD_HP_INTERFACE_Y + (1 - self.ratio_hp) * SIZE_HP_INTERFACE_Y,
-                          SIZE_HP_INTERFACE_X, SIZE_HP_INTERFACE_Y * self.ratio_hp),
-                         border_radius=3)
-        pygame.draw.rect(screen, (0, 0, 0),
-                         (COORD_HP_INTERFACE_X, COORD_HP_INTERFACE_Y, SIZE_HP_INTERFACE_X, SIZE_HP_INTERFACE_Y),
-                         border_radius=3, width=4)
+        pg.draw.rect(screen, "gold",
+                     (COORD_WAVE_INTERFACE_X - 1, COORD_WAVE_INTERFACE_Y + wave[8] / wave[7] * SIZE_WAVE_INTERFACE_Y,
+                      SIZE_WAVE_INTERFACE_X + 8, 6), border_radius=3)
+
+        pg.draw.rect(screen, "gold",
+                     (COORD_PLAYER_LEVEL_INTERFACE_X - 1,
+                      COORD_PLAYER_LEVEL_INTERFACE_Y + player[0, 28] / player[0, 29] * SIZE_PLAYER_LEVEL_INTERFACE_Y,
+                      SIZE_PLAYER_LEVEL_INTERFACE_X + 8, 6), border_radius=3)
+
+        pg.draw.rect(screen, (255, 255, 255),
+                     (COORD_HP_INTERFACE_X, COORD_HP_INTERFACE_Y, SIZE_HP_INTERFACE_X, SIZE_HP_INTERFACE_Y),
+                     border_radius=3)
+
+        pg.draw.rect(screen, "#eb002a",
+                     (COORD_HP_INTERFACE_X, COORD_HP_INTERFACE_Y + (1 - self.ratio_hp) * SIZE_HP_INTERFACE_Y,
+                      SIZE_HP_INTERFACE_X, SIZE_HP_INTERFACE_Y * self.ratio_hp),
+                     border_radius=3)
+        pg.draw.rect(screen, (0, 0, 0),
+                     (COORD_HP_INTERFACE_X, COORD_HP_INTERFACE_Y, SIZE_HP_INTERFACE_X, SIZE_HP_INTERFACE_Y),
+                     border_radius=3, width=4)
 
         # characteristics
         for i, (j, name) in enumerate(zip(self.index_needed_characteristics, self.index_needed_characteristics_name)):
             screen.blit(get_images_for_game()[name],
                         (COORD_CHARACTERISTICS_INTERFACE_X,
                          COORD_CHARACTERISTICS_INTERFACE_Y + INDENT_CHARACTERISTICS_INTERFACE * i))
-            screen.blit(self.font.render(str(self.player[0][j])[:7] if self.player[0][j] > 1e-4 else "0", True, "#3A2980"),
-                        (COORD_CHARACTERISTICS_INTERFACE_X + 33,
-                         COORD_CHARACTERISTICS_INTERFACE_Y + INDENT_CHARACTERISTICS_INTERFACE * i))
+            screen.blit(
+                self.font.render(str(self.player[0][j])[:7] if self.player[0][j] > 1e-4 else "0", True, "#3A2980"),
+                (COORD_CHARACTERISTICS_INTERFACE_X + 33,
+                 COORD_CHARACTERISTICS_INTERFACE_Y + INDENT_CHARACTERISTICS_INTERFACE * i))
 
         # items
         screen.blit(self.items_surface, (0, 0))
